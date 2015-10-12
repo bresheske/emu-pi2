4b9e62ff6a235cb7f89a98f23da1680eaf13bf97
bresheske <bresheske@gmail.com>
2015-10-12 14:59:46 -0500

    Initial Project Commit
    UI and Core for a UWP app.  Successfully requests a fullscreen mode.

---
+++ src/emu-pi2.Core/Logging/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroLog;
+using MetroLog.Targets;
+
+namespace emu_pi2.Core.Logging
+{
+    public class Logger
+    {
+        private static Logger _instance = null;
+
+        private Logger()
+        {
+            LogManagerFactory.DefaultConfiguration.AddTarget(LogLevel.Trace, LogLevel.Fatal, new FileStreamingTarget());
+        }
+
+        public static Logger Current
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Logger();
+                return _instance;
+            }
+        }
+
+        public void Log(string message)
+        {
+            var log = LogManagerFactory.DefaultLogManager.GetLogger(typeof(Logger));
+            log.Trace(message);
+        }
+    }
+}
