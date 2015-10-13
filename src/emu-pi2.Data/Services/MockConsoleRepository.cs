7fd2504d8a67cb26c6839b79ae677956802a836f
bresheske <bresheske@gmail.com>
2015-10-13 08:10:52 -0500

    Better animation, a better grid for wrapping.

--- src/emu-pi2.Data/Services/MockConsoleRepository.cs
+++ src/emu-pi2.Data/Services/MockConsoleRepository.cs
@@ -17,6 +17,10 @@ namespace emu_pi2.Data.Services
                 new Console() {Id = 2, ShortName = "SNES", LongName = "Super Nintendo Entertainment System", BackgroundLink = "Assets/bg-snes.jpg" },
                 new Console() {Id = 3, ShortName = "PS1", LongName = "PlayStation", BackgroundLink = "Assets/bg-ps1.jpg" },
                 new Console() {Id = 3, ShortName = "N64", LongName = "Nintendo 64", BackgroundLink = "Assets/bg-n64.jpg" },
+                new Console() {Id = 1, ShortName = "NES", LongName = "Nintendo Entertainment System", BackgroundLink = "Assets/bg-nes.jpg" },
+                new Console() {Id = 2, ShortName = "SNES", LongName = "Super Nintendo Entertainment System", BackgroundLink = "Assets/bg-snes.jpg" },
+                new Console() {Id = 3, ShortName = "PS1", LongName = "PlayStation", BackgroundLink = "Assets/bg-ps1.jpg" },
+                new Console() {Id = 3, ShortName = "N64", LongName = "Nintendo 64", BackgroundLink = "Assets/bg-n64.jpg" },
             };
         }
     }
